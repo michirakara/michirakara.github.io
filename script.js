69cd0bc8b336c55f8118ca6a59fa1b6f53599ad4
michirakara <naotokonagai@gmail.com>
2023-10-01 19:06:39 -0700

    カスのボタンを作りました

---
+++ script.js
@@ -0,0 +1,10 @@
+function SimonetaGacha(){
+    let elem = document.getElementById("simoneta-result");
+    elem.classList=[];
+    if(Math.random()<0.90){
+        elem.innerHTML="ざんねん";
+    }else{
+        elem.innerHTML="ちんこ";
+        elem.classList=["success"];
+    }
+}
