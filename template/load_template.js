9beee029c289b7c836944e923c51a30997e509f5
michirakara <naotokonagai@gmail.com>
2023-10-19 18:03:11 -0700

    インクルード化できてるかな

---
+++ template/load_template.js
@@ -0,0 +1,10 @@
+const includeTemplate= new XMLHttpRequest();
+includeTemplate.open("GET","/template/template.html",true);
+includeTemplate.onreadystatechange=function () {
+    if(includeTemplate.readyState===4 && includeTemplate.status===200){
+        const templateHTML=includeTemplate.responseText;
+        var insertDiv=document.getElementsById("contents");
+        insertDiv.insertAdjacentHTML('afterbegin',templateHTML);
+    }
+};
+includeTemplate.send();
