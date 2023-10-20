c756df77e238b0aab2675adfe27f486bc11c9b40
michirakara <naotokonagai@gmail.com>
2023-10-19 18:28:37 -0700

    typoしてた

--- template/load_template.js
+++ template/load_template.js
@@ -1,5 +1,5 @@
 const includeTemplate= new XMLHttpRequest();
-includeTemplate.open("GET","https://michirakara.github.io/template/template.html",true);
+includeTemplate.open("GET","/template/template.html",true);
 includeTemplate.onreadystatechange=function () {
     if(includeTemplate.readyState===4 && includeTemplate.status===200){
         const templateHTML=includeTemplate.responseText;
