69084502984397e29e2ace5da7807cd2edf8fb87
michirakara <naotokonagai@gmail.com>
2023-10-19 18:29:58 -0700

    ついにできた？

--- template/load_template.js
+++ template/load_template.js
@@ -3,7 +3,7 @@ includeTemplate.open("GET","/template/template.html",true);
 includeTemplate.onreadystatechange=function () {
     if(includeTemplate.readyState===4 && includeTemplate.status===200){
         const templateHTML=includeTemplate.responseText;
-        var insertDiv=document.getElementsById("contents");
+        var insertDiv=document.getElementById("contents");
         insertDiv.insertAdjacentHTML('afterbegin',templateHTML);
     }
 };
